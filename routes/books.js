5c9432cfb17faa07f0421ea423b1171f404f908c
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-11-07 13:38:17 -0800

    fixed mime type bug

--- routes/books.js
+++ routes/books.js
@@ -68,10 +68,9 @@ async function renderNewPage(res, book, hasError = false) {
 function saveCover(book, coverEncoded) {
     if (coverEncoded == null) return;
     const cover = JSON.parse(coverEncoded);
-    if (cover != null && imageMimeTypes.includes(cover.type)) {
         book.coverImage = new Buffer.from(cover.data, 'base64');
         book.coverImageType = cover.type;
-    }
+    
 };
 
 module.exports = router;
